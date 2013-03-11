034fe62876a28819b37a6c54fe65c14348b26f8e
Gregory Gay <greggay777@gmail.com>
2013-03-11 15:23:28 -0400

    Superior Detailing using Ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperiorDetailing::Application.config.secret_token = '62d526373d7c72b92fef3fd4cd64810a8c7fed92a7aaa19475271f53fad770480122cc550c35d745f343f11f69152ac5478e855ddff83755d51d2e9810a3922d'
