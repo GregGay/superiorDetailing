6f3813002b1fcb2352c4c83b584237b18a72104d
Gregory Gay <greggay777@gmail.com>
2013-04-18 16:45:17 -0400

    my appointments and users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,35 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316025840) do
+ActiveRecord::Schema.define(:version => 20130412204024) do
+
+  create_table "appointments", :force => true do |t|
+    t.string   "package"
+    t.string   "package2"
+    t.string   "package3"
+    t.string   "package4"
+    t.string   "package5"
+    t.string   "package6"
+    t.string   "package7"
+    t.string   "package8"
+    t.string   "package9"
+    t.string   "package10"
+    t.string   "date"
+    t.string   "date2"
+    t.string   "date3"
+    t.string   "date4"
+    t.string   "date5"
+    t.string   "date6"
+    t.string   "date7"
+    t.string   "date8"
+    t.string   "date9"
+    t.string   "date10"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "appointments", ["user_id", "created_at"], :name => "index_appointments_on_user_id_and_created_at"
 
   create_table "users", :force => true do |t|
     t.string   "firstname"
