b0126427d17b469a82cbbefdaeb45ce6d8c69500
Gregory Gay <greggay777@gmail.com>
2013-04-18 16:51:05 -0400

    My routes are now working correctly...FINALLY

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,20 @@
 SuperiorDetailing::Application.routes.draw do
   resources :users
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions,     only: [:new, :create, :destroy]
+  resources :appointments, only: [:create, :destroy]
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
-
+  
   root to: 'static_pages#home'
-
-  match '/signup',  to: 'users#new'
   
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   
-  #get "static_pages/home"
-  #get "static_pages/help"
-  #get "static_pages/about"
-  #get "static_pages/contact"
-
-  #get "static_pages/..."
-
+  #match '/appointment', to: 'users#destroy', via: :delete
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
