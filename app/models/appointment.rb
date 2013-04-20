507fa6517284d307a89ab4ebeed4c873e03c706d
Gregory Gay <greggay777@gmail.com>
2013-04-19 22:49:24 -0400

    Not so much redundancy now, fixed a few problems with the validation as well

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,9 @@ class Appointment < ActiveRecord::Base
   attr_accessible :date, :date2, :date3, :date4, :date5,:date6, :date7, :date8, :package, :package2, :package3, :package4, :package5, :package6, :package7, :package8
   belongs_to :user
 
+###PACKAGE###
+  validates :package, presence: true
+
 ###USER_ID###
   validates :user_id, presence: true
 
