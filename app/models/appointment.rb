a65afd664fbf33861f85c96c5f7dd7155526b6e5
Gregory Gay <greggay777@gmail.com>
2013-04-19 23:57:15 -0400

    Validation is now working correctly, FINALLY

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,6 +5,16 @@ class Appointment < ActiveRecord::Base
 ###PACKAGE###
   validates :package, presence: true
 
+###DATE###
+  validates :date, presence: { :if => :date2? && :date3? && :date4? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
+  validates :date2, presence: { :if => :date? && :date3? && :date4? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
+  validates :date3, presence: { :if => :date? && :date2? && :date4? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
+  validates :date4, presence: { :if => :date? && :date2? && :date3? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
+  validates :date5, presence: { :if => :date? && :date2? && :date3? && :date4? && :date6? && :date7? && :date8? }, uniqueness: true
+  validates :date6, presence: { :if => :date? && :date2? && :date3? && :date4? && :date5? && :date7? && :date8? }, uniqueness: true
+  validates :date7, presence: { :if => :date? && :date2? && :date3? && :date4? && :date5? && :date6? && :date8? }, uniqueness: true
+  validates :date8, presence: { :if => :date? && :date2? && :date3? && :date4? && :date5? && :date6? && :date7? }, uniqueness: true
+
 ###USER_ID###
   validates :user_id, presence: true
 
