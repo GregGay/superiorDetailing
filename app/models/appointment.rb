d9328fd0e2a35c064cbe4376ee49f6d282c89aa9
Gregory Gay <greggay777@gmail.com>
2013-04-21 08:16:09 -0400

    Validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,19 +1,28 @@
 class Appointment < ActiveRecord::Base
-  attr_accessible :date, :date2, :date3, :date4, :date5,:date6, :date7, :date8, :package, :package2, :package3, :package4, :package5, :package6, :package7, :package8
+  attr_accessible :date, :date2, :date3, :date4, :date5,:date6, :date7, :date8, :package, :package2
   belongs_to :user
 
 ###PACKAGE###
   validates :package, presence: true
 
 ###DATE###
-  validates :date, presence: { :if => :date2? && :date3? && :date4? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
-  validates :date2, presence: { :if => :date? && :date3? && :date4? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
-  validates :date3, presence: { :if => :date? && :date2? && :date4? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
-  validates :date4, presence: { :if => :date? && :date2? && :date3? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
-  validates :date5, presence: { :if => :date? && :date2? && :date3? && :date4? && :date6? && :date7? && :date8? }, uniqueness: true
-  validates :date6, presence: { :if => :date? && :date2? && :date3? && :date4? && :date5? && :date7? && :date8? }, uniqueness: true
-  validates :date7, presence: { :if => :date? && :date2? && :date3? && :date4? && :date5? && :date6? && :date8? }, uniqueness: true
-  validates :date8, presence: { :if => :date? && :date2? && :date3? && :date4? && :date5? && :date6? && :date7? }, uniqueness: true
+  #validates :date, presence: { :if => :date2? && :date3? && :date4? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
+  #validates :date2, presence: { :if => :date? && :date3? && :date4? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
+  #validates :date3, presence: { :if => :date? && :date2? && :date4? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
+  #validates :date4, presence: { :if => :date? && :date2? && :date3? && :date5? && :date6? && :date7? && :date8? }, uniqueness: true
+  #validates :date5, presence: { :if => :date? && :date2? && :date3? && :date4? && :date6? && :date7? && :date8? }, uniqueness: true
+  #validates :date6, presence: { :if => :date? && :date2? && :date3? && :date4? && :date5? && :date7? && :date8? }, uniqueness: true
+  #validates :date7, presence: { :if => :date? && :date2? && :date3? && :date4? && :date5? && :date6? && :date8? }, uniqueness: true
+  #validates :date8, presence: { :if => :date? && :date2? && :date3? && :date4? && :date5? && :date6? && :date7? }, uniqueness: true
+
+  validates :date, uniqueness: true
+  validates :date2, uniqueness: true
+  validates :date3, uniqueness: true
+  validates :date4, uniqueness: true
+  validates :date5, uniqueness: true
+  validates :date6, uniqueness: true
+  validates :date7, uniqueness: true
+  validates :date8, uniqueness: true
 
 ###USER_ID###
   validates :user_id, presence: true
